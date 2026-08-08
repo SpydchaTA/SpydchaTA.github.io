c4daf69d3d6d610fbe3d219631d573e563308ea2
SpydchaTA <sporestudios9@gmail.com>
2026-08-08 17:36:45 -0600

    pagina de prueba + pagina de villanos

---
+++ abysspvpStore/sidebar.php
@@ -0,0 +1,26 @@
+  <aside class="sidebar">
+    <div class="logo">
+      <h2>AbyssPvP</h2>
+    </div>
+    <nav class="nav">
+      <h3>Tienda</h3>
+      <a href="#" class="active">Inicio</a>
+
+      <h3>Rangos</h3>
+      <a href="Globales/">Globales</a>
+      <a href="#">Mejoras</a>
+
+      <h3>HCF</h3>
+      <a href="#">Oil</a>
+      <a href="#">Perks</a>
+      <a href="#">Pro League</a>
+      <a href="#">Ability Items</a>
+
+      <h3>KitMap - Pots</h3>
+      <a href="#">Oil</a>
+      <a href="#">Perks</a>
+      <a href="#">Pro League</a>
+      <a href="#">Ability Items</a>
+
+    </nav>
+  </aside>
